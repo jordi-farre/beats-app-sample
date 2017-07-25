dfc2061b8698b450694d386d3fdc118535cc25e4
Jordi Farré <jordi.farre@mango.com>
2017-07-25 14:54:29 +0200

    connect to an external server to test output

--- src/main/java/beats/sample/Application.java
+++ src/main/java/beats/sample/Application.java
@@ -2,8 +2,12 @@ package beats.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
@@ -11,7 +15,11 @@ public class Application {
 
     @RequestMapping(value = "/", produces = "text/html")
     public String home() {
-        return "<html><body><span>Test page</span></body></html>";
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.TEXT_HTML);
+        HttpEntity<String> httpEntity = new HttpEntity<>("<html><body><span>Test page</span></body></html>", httpHeaders);
+        return restTemplate.postForEntity("http://httpbin.org/post", httpEntity, String.class).getBody();
     }
 
     public static void main(String[] args) {
