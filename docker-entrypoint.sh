5115a453bd7f1060a9d61c18cfd79674ac6ca1a5
Jordi Farré <jordi.farre@mango.com>
2017-07-25 13:18:48 +0200

    added configuration to use application using elastic stack

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
+#!/bin/sh
 
+java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -jar /app.jar &
 ${PACKETBEAT_HOME}/packetbeat -e -v -c ${PACKETBEAT_HOME}/packetbeat.yml
-
-java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -jar /app.jar
