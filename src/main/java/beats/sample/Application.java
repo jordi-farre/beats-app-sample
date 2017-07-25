9d21005d06dec680017a651e56c32a89d6c15aad
Jordi Farré <jordi.farre@mango.com>
2017-07-25 17:55:14 +0200

    improve readiness of dockerfile

--- src/main/java/beats/sample/Application.java
+++ src/main/java/beats/sample/Application.java
@@ -1,5 +1,6 @@
 package beats.sample;
 
+import org.apache.commons.logging.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpEntity;
@@ -9,17 +10,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 @RestController
 public class Application {
+
+    private Logger LOGGER = Logger.getLogger(Application.class.getName());
 
     @RequestMapping(value = "/", produces = "text/html")
     public String home() {
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(MediaType.TEXT_HTML);
-        HttpEntity<String> httpEntity = new HttpEntity<>("<html><body><span>Test page</span></body></html>", httpHeaders);
-        return restTemplate.postForEntity("http://httpbin.org/post", httpEntity, String.class).getBody();
+        httpHeaders.setContentType(MediaType.TEXT_PLAIN);
+        HttpEntity<String> httpEntity = new HttpEntity<>("testing", httpHeaders);
+        String result = restTemplate.postForEntity("http://httpbin.org/post", httpEntity, String.class).getBody();
+        LOGGER.info("Result to log: " + result);
+        return "<html><body><span>Test page</span></body></html>";
     }
 
     public static void main(String[] args) {
