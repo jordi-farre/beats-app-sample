e62c1c00fa0e26378f1d58191876b38e186b9ffa
Jordi Farré <jordi.farre@mango.com>
2017-07-27 14:52:15 +0200

    added metricbeat to beats app

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -jar /app.jar &
-${PACKETBEAT_HOME}/packetbeat -e -v -c ${PACKETBEAT_HOME}/packetbeat.yml
+${PACKETBEAT_HOME}/packetbeat -e -v -c ${PACKETBEAT_HOME}/packetbeat.yml &
+${METRICBEAT_HOME}/metricbeat -e -v -c ${METRICBEAT_HOME}/metricbeat.yml
+
