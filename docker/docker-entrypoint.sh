285d8b9d889398771158e04af683e6fab2777274
Jordi Farré <jordi.farre@mango.com>
2017-07-27 11:30:41 +0200

    upgrading packetbeat version

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -jar /app.jar &
-${PACKETBEAT_HOME}/packetbeat -e -v -c ${PACKETBEAT_HOME}/packetbeat.yml
+${PACKETBEAT_HOME}/packetbeat -e -v -c ${PACKETBEAT_HOME}/packetbeat.yml -d "publish"
